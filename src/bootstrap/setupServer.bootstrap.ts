eaca4a3128e146292f19882363377fe6d88a6e6d
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-03-09 13:59:37 -0300

    defining standard middlewares in ChatServer class

--- src/bootstrap/setupServer.bootstrap.ts
+++ src/bootstrap/setupServer.bootstrap.ts
@@ -7,3 +7,37 @@ import compression from 'compression';
 import cookieSession from 'cookie-session';
 import 'express-async-errors';
 import { config } from '../config';
+
+// SOCKETS configuration
+
+export class ChatServer {
+
+  private app: Application;
+
+  constructor(app: Application) {
+    this.app = app;
+  }
+
+  public start(): void {
+    this.standardMiddleware(this.app);
+  }
+
+  private securityMiddleware(app: Application): void {
+    //
+  }
+
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ extended: true, limit: '50mb'}));
+  }
+
+  private async startServer(app: Application): Promise<void> {
+    //
+  }
+
+  private startHttpServer(httpServer: http.Server): void {
+    //
+  }
+
+}
