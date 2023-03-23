fd0eeee48c5c0bda794d2c44643e203f5f049ca8
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-03-23 14:01:40 -0300

    implementation of userSchema and interfaces auth ok and first steps with authSchema

--- src/features/user/models/user.schema.ts
+++ src/features/user/models/user.schema.ts
@@ -1,7 +1,36 @@
 import { IUserDocument } from '../interfaces/userDocument.interface';
 import mongoose, { model, Model, Schema } from 'mongoose';
 
-const userSchema: Schema = new Schema({});
+const userSchema: Schema = new Schema({
+  authId: { type: mongoose.Schema.Types.ObjectId, ref: 'Auth' },
+  profilePicture: { type: String, default: '' },
+  postsCount: { type: Number, default: 0 },
+  followersCount: { type: Number, default: 0 },
+  followingCount: { type: Number, default: 0 },
+  passwordResetToken: { type: String, default: '' },
+  passwordResetExpires: { type: Number },
+  blocked: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  blockedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  notifications: {
+    messages: { type: Boolean, default: true },
+    rections: { type: Boolean, default: true },
+    comments: { type: Boolean, default: true },
+    follows: { type: Boolean, default: true }
+  },
+  social: {
+    facebook: { type: String, default: '' },
+    instagram: { type: String, default: '' },
+    twitter: { type: String, default: '' },
+    youtube: { type: String, default: '' }
+  },
+  work: { type: String, default: '' },
+  school: { type: String, default: ' ' },
+  location: { type: String, default: '' },
+  quote: { type: String, default: '' },
+  bgImageVersion: { type: String, default: '' },
+  bdImageId: { type: String, default: '' }
+});
 
 const UserModel: Model<IUserDocument> = model<IUserDocument>('User', userSchema, 'User');
 export { UserModel };
+
