61b820ea006b34a0ae0272d24e76b3ec0f96e9d0
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-04-04 14:57:20 -0400

    first steps with Signup Controller

--- src/features/auth/controllers/signup.ts
+++ src/features/auth/controllers/signup.ts
@@ -1,3 +1,48 @@
-export class SignUp {
+import { ObjectId } from 'mongodb';
+import { Request, Response } from 'express';
+import { joiValidation } from '@decorators/joi-validation.decorators';
+import { signupSchema } from '@auth/schemes/signup';
+import { authService } from '@services/db/auth.service';
+//import { UserCache } from '@services/redis/user.cache';
+import { BadRequestError } from '@helpers/errors/badRequestError';
+import { Generators } from '@helpers/generators/generators';
+import { IAuthDocument } from '@auth/interfaces/authDocument.interface';
+import { ISignUpData } from '@auth/interfaces/signUpData.interface';
 
+//const userCache: UserCache = new UserCache();
+
+export class SignUp {
+  @joiValidation(signupSchema)
+  public async create(req: Request, res: Response): Promise<void> {
+    const { username, email, password, avatarColor } = req.body;
+    const checkIfUserExist = await authService.getUserByUsernameOrEmail(username, email);
+    if (checkIfUserExist) {
+      throw new BadRequestError('Invalid credentials for this user');
+    }
+
+    const authObjectId: ObjectId = new ObjectId();
+    const userObjectId: ObjectId = new ObjectId();
+    const uId = `${Generators.generateRandomIntegers(12)}`;
+    const authData: IAuthDocument = SignUp.prototype.signUpData({
+      _id: authObjectId,
+      uId,
+      username,
+      email,
+      password,
+      avatarColor
+    });
+  }
+
+  private signUpData(data: ISignUpData): IAuthDocument {
+    const { _id, username, email, uId, password, avatarColor } = data;
+    return {
+      _id,
+      uId,
+      username: Generators.firstLetterUppercase(username),
+      email: Generators.lowerCase(email),
+      password,
+      avatarColor,
+      createdAt: new Date()
+    } as IAuthDocument;
+  }
 }
