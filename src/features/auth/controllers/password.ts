7ba2804786129b80d8ee4ed9c629c14c93b24b8d
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-04-19 17:25:36 -0400

    Password Controller with create and update process with password

--- src/features/auth/controllers/password.ts
+++ src/features/auth/controllers/password.ts
@@ -1,13 +1,13 @@
-import { Request, Response } from 'express';
-import { config } from '@configs/configEnvs';
 import moment from 'moment';
 import publicIp from 'ip';
+import crypto from 'crypto';
 import HTTP_STATUS from 'http-status-codes';
+import { Request, Response } from 'express';
+import { config } from '@configs/configEnvs';
 import { authService } from '@services/db/auth.service';
 import { IAuthDocument } from '@auth/interfaces/authDocument.interface';
 import { joiValidation } from '@decorators/joi-validation.decorators';
 import { emailSchema, passwordSchema } from '@auth/schemes/emailAndPassword';
-import crypto from 'crypto';
 import { forgotPasswordTemplate } from '@services/emails/templates/forgot-password/forgot-password-template';
 import { resetPasswordTemplate } from '@services/emails/templates/reset-password/reset-password-template';
 import { emailQueue } from '@services/queues/email.queue';
@@ -16,7 +16,6 @@ import { BadRequestError } from '@helpers/errors/badRequestError';
 import { Generators } from '@helpers/generators/generators';
 
 export class Password {
-
   @joiValidation(emailSchema)
   public async create(req: Request, res: Response): Promise<void> {
     const { email } = req.body;
@@ -25,9 +24,8 @@ export class Password {
       throw new BadRequestError('Invalid credentials');
     }
 
-    // cryptography
-    const randomBytes: Buffer = await Promise.resolve(crypto.randomBytes(20));
-    const randomCharacters: string = randomBytes.toString('hex');
+    const randomBytes: Buffer = await Promise.resolve(crypto.randomBytes(20)); // ej: <Buffer a5 8b b5 73 55 8a 77 8c 6a 5a 9a ff 4a 6e 7f a3 f3 17 5f 4e>
+    const randomCharacters: string = randomBytes.toString('hex'); // ej: a58bb573558a778c6a5a9aff4a6e7fa3f3175f4e representa una cadena con números del 0-9 y de la a-f
 
     await authService.updatePasswordToken(`${existingUser._id}`, randomCharacters, Date.now() * 60 * 60 * 1000);
     const resetLink = `${config.CLIENT_URL}/reset-password?token=${randomCharacters}`;
@@ -41,7 +39,6 @@ export class Password {
     const { password, confirmPassword } = req.body;
     const passwordHash = await Generators.hash(password);
     const { token } = req.params;
-    // manejo opcional si no tuvieras el esquema de def. abstraído
     if (password !== confirmPassword) {
       throw new BadRequestError('Passwords do not match');
     }
@@ -49,6 +46,24 @@ export class Password {
     if (!existingUser) {
       throw new BadRequestError('Reset token has expired or invalid.');
     }
+
+    existingUser.password = passwordHash;
+    existingUser.passwordResetExpires = undefined;
+    existingUser.passwordResetToken = undefined;
+    await existingUser.save();
+
+    const templateParams: IResetPasswordParams = {
+      username: existingUser.username,
+      email: existingUser.email,
+      ipaddress: publicIp.address(),
+      date: moment().format('DD/MM/YYYY HH:mm')
+    };
+    const template: string = resetPasswordTemplate.passwordResetConfirmationTemplate(templateParams);
+    emailQueue.addEmailJob('forgotPasswordEmail', {
+      template,
+      receiverEmail: existingUser.email,
+      subject: 'Password Reset Confirmation'
+    });
+    res.status(HTTP_STATUS.OK).json({ message: 'Password successfully updated. ' });
   }
 }
-
