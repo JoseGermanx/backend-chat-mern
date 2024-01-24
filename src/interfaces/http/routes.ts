6742c153ea6d9252a032bfeadd2e7eb23955fb69
dev-miguel-resources <ingenieromiguelch@gmail.com>
2024-01-24 12:21:27 -0300

    currentUser controller completed

--- src/interfaces/http/routes.ts
+++ src/interfaces/http/routes.ts
@@ -2,8 +2,8 @@ import { Request, Response } from 'express';
 import { Application } from 'express';
 import { authRoutes } from '@auth/routes/authRoutes';
 import { serverAdapter } from '@services/queues/base.queue';
-//import { currentUserRoutes } from '@auth/routes/currentRoutes';
-//import { authMiddleware } from '@helpers/middlewares/auth-middleware';
+import { currentUserRoutes } from '@auth/routes/currentRoutes';
+import { authMiddleware } from '@helpers/middlewares/auth-middleware';
 import { config } from '@configs/configEnvs';
 
 export default (app: Application) => {
@@ -13,7 +13,7 @@ export default (app: Application) => {
     app.use(config.BASE_PATH!, authRoutes.routes());
     //app.use(config.BASE_PATH!, authRoutes.signoutRoute());
 
-    //app.use(config.BASE_PATH!, authMiddleware.verifyUser, currentUserRoutes.routes());
+    app.use(config.BASE_PATH!, authMiddleware.verifyUser, currentUserRoutes.routes());
   };
   routes();
 };
